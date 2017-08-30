978c9f860f328ce95b83f8d9bd3e6aacf0811e65
DarminZhou <790471332@qq.com>
2017-08-30 12:25:27 +0800

    first commit

--- src/Array/getMaxLength1.java
+++ src/Array/getMaxLength1.java
@@ -4,5 +4,8 @@ package Array;
  * Created by Administrator on 2017/8/30.
  */
 public class getMaxLength1 {
+    public static void main(String [] args){
+        System.out.println("hah");
 
+    }
 }
