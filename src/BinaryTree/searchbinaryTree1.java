114550b058e48aa4a07c2e5c51c7d4607c03801a
DarminZhou <790471332@qq.com>
2017-10-23 11:25:22 +0800

    new Algorithm commit .

--- src/BinaryTree/searchbinaryTree1.java
+++ src/BinaryTree/searchbinaryTree1.java
@@ -1,29 +1,11 @@
-// 前序遍历  递归
-public void preOrder(Node node){
-      list.add(node);
-      if(node.lchid!=null){
-          preOrder(node.lchid);
-      }
-      if(node.rchid!=null){
-          preOrder(node.rchid);
-      }
-}
+package BinaryTree;
+
+import java.util.Stack;
+
+public class searchbinaryTree1 {
+    // 前序遍历  递归
 
 
-// 前序遍历 非递归
-public void IteratorPreorder(Node node){
-  Stack<Node> stack = new Stack<Node>();
-  if(node != null){
-    stack.push(node);
-    while(!stack.empty()){
-        node=stack.pop();
-        visit(node);
-        if(node.getRight()!=null)
-            stack.push(node.getRight());
-            
-            // 栈后进先出 所以先右后左进栈
-        if(node.getLeft()!=null)
-            stack.push(node.getLeft());
+    private void visit(Node node) {
     }
-  }
 }
---
+++ src/String/IsIdentifierIP.java
@@ -0,0 +1,40 @@
+package String;
+
+import java.util.Scanner;
+
+/**
+ * Created by zdmein on 2017/10/23.
+ * 写一个算法判断某个字符串是不是一个合法的IP地址。
+ */
+public class IsIdentifierIP {
+    public static void main(String [] args){
+        Scanner scanner =new Scanner(System.in);
+        String input=null;
+        System.out.println("请输入待判定字符串：");
+        input=scanner.nextLine();
+        if(isIdentifierIP(input)){
+            System.out.println("is IP");
+        }else {
+            System.out.println("Not is IP");
+        }
+    }
+
+    public static boolean isIdentifierIP(String input){
+        if(input!=null && input.length()>0){
+            String [] ipn=input.split("\\.");
+            if(ipn.length!=4) return false;
+            try {
+                if(Integer.parseInt(ipn[0])>0  && Integer.parseInt(ipn[0])<=255
+                        &&  Integer.parseInt(ipn[1])>=0 && Integer.parseInt(ipn[1])<=255
+                        &&  Integer.parseInt(ipn[2])>=0 && Integer.parseInt(ipn[2])<=255
+                        &&  Integer.parseInt(ipn[3])>=0 && Integer.parseInt(ipn[3])<=255){
+                    return true;
+                }
+
+            }catch (NumberFormatException e){
+                return false;
+            }
+        }
+        return false;
+    }
+}
